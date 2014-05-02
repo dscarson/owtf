91ead509ff29eed9896a49fd1e7073dd161f79aa
Tao Sauvage <sauvage.tao@gmail.com>
2014-05-02 22:00:00 +0200

    [plugin] Create new plugin class for those who run shell commands.

--- framework/plugin/plugins.py
+++ framework/plugin/plugins.py
@@ -1,5 +1,7 @@
 import os
+from framework.lib.general import PluginAbortException, FrameworkAbortException
 from framework.lib.general import WipeBadCharsForFilename as clean_filename
+from framework.lib.general import log
 
 
 # TODO: Checks if already declared elsewhere
@@ -37,9 +39,14 @@ class AbstractPlugin(object):
         """Self-explanatory."""
         # A plugin has a reference to the Core object.
         self.core = core
+        # Keep track of the abort
+        self.framework_abort = False
+        self.plugin_abort = False
+        # Keep track of the elapsed time
+        self.elapsed_time = None
         # A plugin contains several information like a group, a type, etc.
         self.info = None
-        if check_info(plugin_info):
+        if self._check_info(plugin_info):
             self.info = plugin_info
         else:  # The information are not valid, throw something
             # TODO: Create a custom error maybe?
@@ -57,7 +64,7 @@ class AbstractPlugin(object):
         raise NotImplementedError('A plugin MUST implement the run method.')
 
     @staticmethod
-    def check_info(info):
+    def _check_info(info):
         """Check that the information of a plugin is correct."""
         # Check if a group is specified and if it is a valid one.
         if (not 'group' in info or
@@ -72,16 +79,58 @@ class AbstractPlugin(object):
         return True
 
     @classmethod
-    def get_output_dir(cls):
+    def _init_output_dir(cls):
         """Returns the output path of the plugin."""
+        # Retrieve the relative path of the plugin output.
         base_path = ''
         if cls.info['group'] in [GROUP_WEB, GROUP_NET]:
             base_path = cls.core.DB.Target.GetPath('PARTIAL_URL_OUTPUT_PATH')
         elif cls.info['group'] == GROUP_AUX:
             base_path = cls.core.Config.Get('AUX_OUTPUT_PATH')
-        return os.path.join(
+        output_dir = os.path.join(
             base_path,
             os.path.join(clean_filename(cls.info['title']), cls.info['type']))
+        # FULL output path for plugins to use
+        cls.core.DB.Target.SetPath(
+            'PLUGIN_OUTPUT_DIR',
+            os.path.join(os.getcwd(), output_dir))
+        # Force the creation of the directory if it does not exist yet.
+        cls.core.CreateMissingDirs(output_dir)
+        cls.output_dir = output_dir
+
+
+class AbstractRunCommandPlugin(AbstractPlugin):
+    """Abstract plugin that run a shell command."""
+
+    def __init__(self, *args, **kwargs):
+        """Self-explanatory."""
+        AbstractPlugin.__init__(self, *args, **kwargs)
+        self.modified_command = None
+        self.raw_output = None
+
+    def run_command(self, command):
+        """Run the shell command of the plugin."""
+        if not hasattr(self, 'output_dir'):
+            self._init_output_dir()
+        # Keep track of the elapsed time.
+        self.core.Timer.StartTimer('FormatCommandAndOutput')
+        self.modified_command = self.core.Shell.GetModifiedShellCommand(
+            command,
+            self.output_dir)
+        # Run the shell command.
+        try:
+            self.raw_output = self.core.Shell.shell_exec_monitor(
+                modified_command)
+        except PluginAbortException as partial_output:
+            self.raw_output = str(partial_output.parameter)
+            self.plugin_abort = True
+        except FrameworkAbortException as partial_output:
+            self.raw_output = str(partial_output)
+            self.framework_abort = True
+        # Save the elapsed time.
+        self.elapsed_time = self.core.Timer.GetElapsedTimeAsStr(
+            'FormatCommandAndOutput')
+        log('Time=' + self.elapsed_time)
 
 
 class ActivePlugin(AbstractPlugin):
