b3bdf49eb94fdb3338c17c43d8b40b0bffb00adf
Tao Sauvage <sauvage.tao@gmail.com>
2014-05-12 22:10:30 +0200

    [net/active plugin] Update older plugins.

--- plugins/net/active/ftp@PTES-001.py
+++ plugins/net/active/ftp@PTES-001.py
@@ -1,4 +1,5 @@
 """
+
 owtf is an OWASP+PTES-focused try to unite great tools and facilitate pen testing
 Copyright (c) 2011, Abraham Aranguren <name.surname@gmail.com> Twitter: @7a_ http://7-a.org
 All rights reserved.
@@ -17,20 +18,23 @@ modification, are permitted provided that the following conditions are met:
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Plugin for probing ftp
+Plugin for probing ftp.
+
 """
 
-DESCRIPTION = " FTP Probing "
 
-def run(Core, PluginInfo):
-    #Core.Config.Show()
-    #print "Content="+Content
-    return Core.PluginHelper.CommandDump('Test Command', 'Output', Core.DB.Resource.GetResources('FtpProbeMethods'), PluginInfo, []) # No previous output
+from framework.plugin.plugins import ActivePlugin
+
+
+class FTPProbingPlugin(ActivePlugin):
+    """FTP Probing."""
+
+    RESOURCES = 'FtpProbeMethods'
